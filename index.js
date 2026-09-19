21927c71496180604cbeb4016dc7e53541382776
sahadathussain12 <sahadathussain872856@gmail.com>
2026-09-19 19:00:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+const uri = process.env.MONGODB_URI;
+const port = process.env.PORT
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("wanderlust");
+    const destinationsCollection = db.collection("destinations");
+
+
+    app.get('/destinations', async (req,res)=>{
+        const destination = await destinationsCollection.find().toArray();
+        res.send(destination);
+    })
+
+    app.post('/destinations', (req,res) => {
+       const destinationsData = req.body;
+       console.log(destinationsData);
+       const result = destinationsCollection.insertOne(destinationsData)
+       res.send(result);
+    })
+
+
+
+
+
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, ()=> {
+  console.log(`Server is running on port ${port}`)
+})
